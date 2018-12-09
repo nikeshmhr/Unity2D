4d963f597b0dfb3498ee9732e26cb5ca8e265f6a
Nikesh Maharjan <nmaharjan@virmedica.com>
2018-12-09 21:12:23 +0545

    * Initial Commit.

---
+++ Assets/Scripts/BoundaryChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryChecker : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Trigger Entered: " + collision.GetComponent<Collider2D>().name);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        // Destroy GameObjects that exit the Boundary (Box) trigger collider
+        Debug.Log("Trigger Exited: " + collision.GetComponent<Collider2D>().name);
+
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/FollowMe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script that moves the gameObject its attached to at certain given speed
+public class FollowMe : MonoBehaviour {
+
+    // The speed to move our object
+    public float speed = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.up * speed * Time.deltaTime, Space.World);
+	}
+}
